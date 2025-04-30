de0d558be5c94145f0dce4c045be8dea90df9031
Loris De Biasi <loris.debiasi@gmail.com>
2025-04-30 09:18:54 +0200

    adding argparse description

--- 1-batch.py
+++ 1-batch.py
@@ -24,10 +24,10 @@ def split_dir_into_batches(input_dir, output_dir, nb_batches):
 		print(f"Copied {src} to {dst}")
 
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser(description='')
-	parser.add_argument('input', metavar='IN', type=str, help='')
-	parser.add_argument('output', metavar='OUT', type=str, help='')
-	parser.add_argument('nb_batchs', metavar='NB', type=int, help='')
+	parser = argparse.ArgumentParser(description='Split a directory into batches')
+	parser.add_argument('input', metavar='IN', type=str, help='Folder where files are stored')
+	parser.add_argument('output', metavar='OUT', type=str, help='Folder where files will be copied')
+	parser.add_argument('nb_batchs', metavar='NB', type=int, help='Number of batches to create')
 	args = parser.parse_args()
 
 	split_dir_into_batches(args.input, args.output, args.nb_batchs)
